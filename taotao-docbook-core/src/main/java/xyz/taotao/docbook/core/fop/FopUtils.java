4bb59fb08a304efca09839b1ab74177aa751a426
王金涛 <wangjintao@ecloude.com>
2022-04-19 14:58:32 +0800

    添加 pom 的 许可证、开发者、scm 信息，修正许可证错误

--- taotao-docbook-core/src/main/java/xyz/taotao/docbook/core/fop/FopUtils.java
+++ taotao-docbook-core/src/main/java/xyz/taotao/docbook/core/fop/FopUtils.java
@@ -3,14 +3,14 @@
  * This file is part of taotao-docbook.
  *
  * taotao-docbook is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Lesser Public License as published by the Free Software Foundation, either
+ * GNU Lesser General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
  *
  * taotao-docbook is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE. See the GNU Lesser Public License for more details.
+ * PURPOSE. See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser Public License along with taotao-docbook. If
+ * You should have received a copy of the GNU Lesser General Public License along with taotao-docbook. If
  * not, see <https://www.gnu.org/licenses/>.
  */
 
